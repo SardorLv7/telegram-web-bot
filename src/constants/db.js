11b352b6bce7bf32c3423b68ec6492ebae47fcbb
SardorLv7 <uzbtiktokhouses@gmail.com>
2023-07-25 09:19:16 +0500

    added new db

--- src/constants/db.js
+++ src/constants/db.js
@@ -4,57 +4,107 @@ export function getData() {
 			title: 'Hod Dog',
 			price: 17.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F3gf746AKRbWNjB8OCoEB&w=3840&q=75',
+				'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F09%2F09%2F268494_Basic-Air-Fryer-Hot-Dogs_Buckwheat-Queen_SERP_5844319_original-4x3-1.jpg&tbnid=hWPD_D2bNq1MqM&vet=12ahUKEwjA8eue_qiAAxXmExAIHUWBAusQMygBegUIARCGAg..i&imgrefurl=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F268494%2Fbasic-air-fryer-hot-dogs%2F&docid=f7dy9gLplzaZnM&w=1500&h=1125&q=hot%20dog&ved=2ahUKEwjA8eue_qiAAxXmExAIHUWBAusQMygBegUIARCGAg ',
 			id: 1,
 		},
 		{
-			title: 'FrontEnd dasturlash',
+			title: 'Cheesburger',
 			price: 15,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F65rcPxsLT9ysJDisXF80&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdamndelicious.net%2F2022%2F08%2F12%2Fthe-best-ever-cheeseburger%2F&psig=AOvVaw1nJYt2u1hLcGVeC3OIRGSs&ust=1690344626105000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJjJka7-qIADFQAAAAAdAAAAABAE',
 			id: 2,
 		},
 		{
-			title: 'VueJS',
+			title: 'Pizza',
 			price: 13.5,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2F54vR0DStGoFuZBVkFwWQ&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fpizza&psig=AOvVaw1Rj1GICy9Ctz-Im2Nl_gb0&ust=1690344662694000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJCdrcH-qIADFQAAAAAdAAAAABAE',
 			id: 3,
 		},
 		{
-			title: 'NodeJS',
+			title: 'Kebab',
 			price: 13.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FmDcwhbguQpyM74jb18M5&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcookingorgeous.com%2Fblog%2Flamb-shish-kebab%2F&psig=AOvVaw327SpxA6G3RoecrPB98kPH&ust=1690344706845000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJDprtT-qIADFQAAAAAdAAAAABAE',
 			id: 4,
 		},
 		{
-			title: 'NestJS',
+			title: 'Fried chicken',
 			price: 12.5,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FxN7aKkLMQUuD72ikvC1H&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tasteofhome.com%2Frecipes%2Fcrispy-fried-chicken%2F&psig=AOvVaw1LbZ0oYIonfJVI7XPQqwji&ust=1690344739144000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMDx9uP-qIADFQAAAAAdAAAAABAE',
 			id: 5,
 		},
 		{
-			title: 'NextJS',
+			title: 'French fries',
 			price: 12.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FPe94ubZPSCHgXDIPBxTr&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thespruceeats.com%2Fhomemade-french-fries-6749413&psig=AOvVaw1Ov9HWSDepp7MHDI_edcIL&ust=1690344777309000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCPDik_b-qIADFQAAAAAdAAAAABAE',
 			id: 6,
 		},
 		{
-			title: 'TypeScript',
+			title: 'Taco',
 			price: 22.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FfZ3plnBeRQ2hJtZNbhNE&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bettycrocker.com%2Frecipes%2Feasy-beef-tacos%2F426c261d-b2eb-4e22-9cba-0066f3335591&psig=AOvVaw082UQEFfJNYxpol-ILphTf&ust=1690344810466000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCNCW8oX_qIADFQAAAAAdAAAAABAE',
 			id: 7,
 		},
 		{
-			title: 'TensorflowJS',
+			title: 'Doner Kebab',
 			price: 28.99,
 			Image:
-				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FtIMgibghQveiDeJeXDPI&w=3840&q=75',
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.myfoodandfamily.com%2Frecipe%2F221073%2Fdoner-kebab-turkish-style-kebab-wrap&psig=AOvVaw3kA5ZRfk5qJx726bggxUOr&ust=1690344844112000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCKj1iJb_qIADFQAAAAAdAAAAABAK',
 			id: 8,
 		},
+		{
+			title: 'Sandwich',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnatashaskitchen.com%2Fgrilled-cheese-sandwich%2F&psig=AOvVaw1-C7dBkAH7cXdoFFT8uPgj&ust=1690344906579000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMidqbT_qIADFQAAAAAdAAAAABAE',
+			id: 9,
+		},
+		{
+			title: 'Popcorn',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnomoneynotime.com.au%2Fhealthy-easy-recipes%2Fquick-easy-popcorn&psig=AOvVaw2T1YATj04Byk_q9hKcxDpU&ust=1690344940697000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCKjTsMT_qIADFQAAAAAdAAAAABAJ',
+			id: 10,
+		},
+		{
+			title: 'Ice cream',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsugarspunrun.com%2Fice-cream-cone-cupcakes%2F&psig=AOvVaw0o6z3wJLAd4q10GGN24ldH&ust=1690344987101000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMiwhNr_qIADFQAAAAAdAAAAABAJ',
+			id: 11,
+		},
+		{
+			title: 'Hamburger',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.aspicyperspective.com%2Fbest-hamburger-patty-recipe%2F&psig=AOvVaw0eNFRsBYBGpbcP4AKaqH45&ust=1690345024302000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCLj87uv_qIADFQAAAAAdAAAAABAE',
+			id: 12,
+		},
+		{
+			title: 'Onion rings',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fhouseofnasheats.com%2Fonion-rings%2F&psig=AOvVaw08LabvDLWAoIx7_o4VOtjk&ust=1690345059102000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCNChrfz_qIADFQAAAAAdAAAAABAE',
+			id: 13,
+		},
+		{
+			title: 'Lavash',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.taste.com.au%2Frecipes%2Fvegie-lavash-spirals%2Fd4b8bccb-001d-476d-8681-2df21a62fdbc&psig=AOvVaw0BGNO2YvQfCv3IBSMHC0Q7&ust=1690345087705000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCOCZ9omAqYADFQAAAAAdAAAAABAJ',
+			id: 14,
+		},
+		{
+			title: 'Donar',
+			price: 28.99,
+			Image:
+				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fmasterfoodtashkent%2Fposts%2Ftovuqli-donar-juda-yaxshi-agar-hali-tatib-kormagan-bolsangiz-buni-togrilashning-%2F626974138181416%2F%3Flocale%3Dms_MY&psig=AOvVaw3TGvmNIgOHHMdKVRrVbDp2&ust=1690345131320000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCPjD0J6AqYADFQAAAAAdAAAAABAe',
+			id: 15,
+		},
+
 	];
 }
