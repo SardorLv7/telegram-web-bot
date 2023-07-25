247b1c8812183e762417f83758d33aac2427e48e
SardorLv7 <uzbtiktokhouses@gmail.com>
2023-07-25 09:52:24 +0500

    imgs

--- src/constants/db.js
+++ src/constants/db.js
@@ -4,91 +4,91 @@ export function getData() {
 			title: 'Hod Dog',
 			price: 17.99,
 			Image:
-				'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F09%2F09%2F268494_Basic-Air-Fryer-Hot-Dogs_Buckwheat-Queen_SERP_5844319_original-4x3-1.jpg&tbnid=hWPD_D2bNq1MqM&vet=12ahUKEwjA8eue_qiAAxXmExAIHUWBAusQMygBegUIARCGAg..i&imgrefurl=https%3A%2F%2Fwww.allrecipes.com%2Frecipe%2F268494%2Fbasic-air-fryer-hot-dogs%2F&docid=f7dy9gLplzaZnM&w=1500&h=1125&q=hot%20dog&ved=2ahUKEwjA8eue_qiAAxXmExAIHUWBAusQMygBegUIARCGAg ',
+				'https://static.toiimg.com/thumb/54478818.cms?width=1200&height=900',
 			id: 1,
 		},
 		{
 			title: 'Cheesburger',
 			price: 15,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdamndelicious.net%2F2022%2F08%2F12%2Fthe-best-ever-cheeseburger%2F&psig=AOvVaw1nJYt2u1hLcGVeC3OIRGSs&ust=1690344626105000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJjJka7-qIADFQAAAAAdAAAAABAE',
+				'https://www.kitchensanctuary.com/wp-content/uploads/2021/05/Double-Cheeseburger-square-FS-42.jpg',
 			id: 2,
 		},
 		{
 			title: 'Pizza',
 			price: 13.5,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fpizza&psig=AOvVaw1Rj1GICy9Ctz-Im2Nl_gb0&ust=1690344662694000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJCdrcH-qIADFQAAAAAdAAAAABAE',
+				'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg',
 			id: 3,
 		},
 		{
 			title: 'Kebab',
 			price: 13.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcookingorgeous.com%2Fblog%2Flamb-shish-kebab%2F&psig=AOvVaw327SpxA6G3RoecrPB98kPH&ust=1690344706845000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJDprtT-qIADFQAAAAAdAAAAABAE',
+				'https://cookingorgeous.com/wp-content/uploads/2021/06/lamb-shish-kebab-20.jpg',
 			id: 4,
 		},
 		{
 			title: 'Fried chicken',
 			price: 12.5,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tasteofhome.com%2Frecipes%2Fcrispy-fried-chicken%2F&psig=AOvVaw1LbZ0oYIonfJVI7XPQqwji&ust=1690344739144000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMDx9uP-qIADFQAAAAAdAAAAABAE',
+				'https://twoplaidaprons.com/wp-content/uploads/2023/02/Korean-fried-chicken-wings-on-a-plate-thumbnail-shot-500x375.jpg',
 			id: 5,
 		},
 		{
 			title: 'French fries',
 			price: 12.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thespruceeats.com%2Fhomemade-french-fries-6749413&psig=AOvVaw1Ov9HWSDepp7MHDI_edcIL&ust=1690344777309000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCPDik_b-qIADFQAAAAAdAAAAABAE',
+				'https://static.toiimg.com/thumb/54659021.cms?imgsize=275086&width=800&height=800',
 			id: 6,
 		},
 		{
 			title: 'Taco',
 			price: 22.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bettycrocker.com%2Frecipes%2Feasy-beef-tacos%2F426c261d-b2eb-4e22-9cba-0066f3335591&psig=AOvVaw082UQEFfJNYxpol-ILphTf&ust=1690344810466000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCNCW8oX_qIADFQAAAAAdAAAAABAE',
+				'https://upload.wikimedia.org/wikipedia/commons/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg',
 			id: 7,
 		},
 		{
 			title: 'Doner Kebab',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.myfoodandfamily.com%2Frecipe%2F221073%2Fdoner-kebab-turkish-style-kebab-wrap&psig=AOvVaw3kA5ZRfk5qJx726bggxUOr&ust=1690344844112000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCKj1iJb_qIADFQAAAAAdAAAAABAK',
+				'https://www.slimmingeats.com/blog/wp-content/uploads/2021/04/doner-kebab-fakeaway-14-735x735.jpg',
 			id: 8,
 		},
 		{
 			title: 'Sandwich',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnatashaskitchen.com%2Fgrilled-cheese-sandwich%2F&psig=AOvVaw1-C7dBkAH7cXdoFFT8uPgj&ust=1690344906579000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMidqbT_qIADFQAAAAAdAAAAABAE',
+				'https://img.taste.com.au/OX2Wle32/taste/2016/11/marinated-chicken-club-sandwich-1970-1.jpeg',
 			id: 9,
 		},
 		{
 			title: 'Popcorn',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fnomoneynotime.com.au%2Fhealthy-easy-recipes%2Fquick-easy-popcorn&psig=AOvVaw2T1YATj04Byk_q9hKcxDpU&ust=1690344940697000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCKjTsMT_qIADFQAAAAAdAAAAABAJ',
+				'https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?cs=srgb&dl=pexels-pixabay-33129.jpg&fm=jpg',
 			id: 10,
 		},
 		{
 			title: 'Ice cream',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsugarspunrun.com%2Fice-cream-cone-cupcakes%2F&psig=AOvVaw0o6z3wJLAd4q10GGN24ldH&ust=1690344987101000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCMiwhNr_qIADFQAAAAAdAAAAABAJ',
+				'https://www.chewoutloud.com/wp-content/uploads/2013/08/cookies-and-cream-in-white-bowl.jpg',
 			id: 11,
 		},
 		{
 			title: 'Hamburger',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.aspicyperspective.com%2Fbest-hamburger-patty-recipe%2F&psig=AOvVaw0eNFRsBYBGpbcP4AKaqH45&ust=1690345024302000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCLj87uv_qIADFQAAAAAdAAAAABAE',
+				'https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe-500x500.jpg',
 			id: 12,
 		},
 		{
 			title: 'Onion rings',
 			price: 28.99,
 			Image:
-				'https://www.google.com/url?sa=i&url=https%3A%2F%2Fhouseofnasheats.com%2Fonion-rings%2F&psig=AOvVaw08LabvDLWAoIx7_o4VOtjk&ust=1690345059102000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCNChrfz_qIADFQAAAAAdAAAAABAE',
+				'https://houseofnasheats.com/wp-content/uploads/2023/03/Homemade-Onion-Rings-Square-1.jpg',
 			id: 13,
 		},
 		{
